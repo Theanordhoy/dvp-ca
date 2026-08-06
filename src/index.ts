f710e6e8f7282251cf39cba01e2fead6876a748c
Theanordhoy <thea.n.k@hotmail.com>
2026-08-06 15:07:58 +0200

    installed express, cors, mysql, typescript, added packages. Created different folders and files. Added interface for user and articles. Created get articles and post articles

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import articleRoutes from "./routes/articles";
+
+dotenv.config();
+
+const app = express(); // This creates our Express application
+const PORT = process.env.PORT || 3000; // This is the port our server will listen on
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/articles", articleRoutes);
+
+// This starts the server and listens for incoming requests
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
